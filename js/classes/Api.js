421b0aa0ea69d90604cb25abb9c76fcf10314080
svetlana-urasinova <urasinova.work@gmail.com>
2022-02-16 20:04:46 +0100

    Now use fetch instead of XMLHttpRequest.

--- js/classes/Api.js
+++ js/classes/Api.js
@@ -1,57 +1,56 @@
 import { Config } from "./Config.js";
 
 export class Api {  
+    static makeRequest (method, data={}, cb = data => data) {
+       const url = Config.get('url'); 
+       const params = { method };
+       if (Object.keys(data).length > 0) {
+            params.headers = {
+                'Content-Type': 'application/json'
+            };
+            params.body = JSON.stringify(data);
+       }
 
-    static makeRequest(method, url, data=null) {
-        return new Promise(function(resolve, reject) {
-            const xhr = new XMLHttpRequest();
-            xhr.open(method, url);
-            xhr.onload = function() {
-                if (this.status >= 200 && this.status < 300) {
-                    resolve(xhr.response);
-                } else {
-                    reject({ 
-                        status: this.status,
-                        statusText: xhr.statusText
-                    })
-                }
-            } 
-            xhr.onerror = function() {
-                console.log("Url is not available. Please check if you have a correct url in /js/config.js");
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
+       return fetch(url, params)
+                .then(response => response.json())
+               .then(data => cb(data));
+    }
+
+    static read(id=null) {
+        const data = id ? { id } : {};
+        return this.makeRequest('GET', data);
+    }
+
+    static add(name) {
+        const data = { name };
+        return this.makeRequest('POST', data, res => {
+            if (/[^\d]/i.test(res)) {
+                console.error(res);
             }
-            xhr.send(data);
         });
     }
 
-    static async read(id=null) {
-        const url = Config.get('url') + 'read' + (id === null ? '' : `/${id}`);
-        const res = await this.makeRequest('GET', url);
-        return JSON.parse(res);
-    }
-
-    static async add(name) {
-        const url = Config.get('url') + 'add';
-        const data = new FormData();
-        data.append('name', name);
-        const res = await this.makeRequest('POST', url, data);
-        return JSON.parse(res);
-    }
-
-    static async edit(id, name=null, completed=null) {
+    static edit(id, name=null, completed=null) {
         if (!id) { return false; }
-        const url = Config.get('url') + 'edit/' + id;
-        const data = Object.entries({ 'name': name ?? '', 'completed': completed ? 1 : 0}).map(el => `${el[0]}=${el[1]}`).join("&");
-        return await this.makeRequest('PUT', url, data);
+        const data = { 
+            id, 
+            'name': name ?? '',
+            'completed': completed ? 1 : 0
+        }
+        return this.makeRequest('PUT', data, res => {
+            if (typeof data === 'string') {
+                console.error(data);
+            }
+        });
     }
     
-    static async delete(id) {
+    static delete(id) {
         if (!id) { return false; }
-        const url = Config.get('url') + 'delete/' + id;
-        const res = await this.makeRequest('DELETE', url);
-        return res;
+        const data = { id };
+        return this.makeRequest('DELETE', data, res => {
+            if (res) {
+                console.error(res);
+            }
+        });
     } 
 }
